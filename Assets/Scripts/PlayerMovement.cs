176349d068d72257b0483f273c318a1009cb9956
Christian Broomes <wigglytroll64@gmail.com>
2025-07-27 13:13:24 -0400

    Revert "More InputManager Stuff"
    This reverts commit d3b98cd7e730d2fd70218874a5caabf45e038906.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,32 +22,30 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (!IsGrounded())
-        {
-            Debug.Log("BRO IS NOT GREOUNDED!!!!!!!!!");
-        }
-        else
-        {
-            Debug.Log("GROIUHWEADHODHWOD");
-        }
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (InputManager.jumpButton.PressedThisFrame() && IsGrounded())
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        Debug.Log(InputManager.rollButton.Active());
     }
+
     void FixedUpdate()
     {
         rb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
         PlayerGravity();
     }
+
     void OnDrawGizmos()
     {
         Gizmos.DrawRay(transform.position, Vector3.down * groundedRayHeight);
     }
+
     public bool IsGrounded()
     {
         return Physics.Raycast(rb.transform.position, Vector3.down, groundedRayHeight, LayerMask.GetMask("Ground"));
     }
+
     void PlayerGravity()
     {
         if (!IsGrounded())
